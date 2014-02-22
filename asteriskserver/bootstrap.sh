d963348fbfca7ca323d981b2283054ebf50bef0b
Karl Anderson <kra@monkey.org>
2014-02-21 18:26:48 -0800

    special casing for virtualbox, bootstrap fixes

--- asteriskserver/bootstrap.sh
+++ asteriskserver/bootstrap.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+set -x # print commands as executed
+
+# Are we in a virtualbox?  Should just have vagrant send this as an arg.
+# Wasn't able to pass the value of config.vm.provider, but could just set it
+# manually.
+virtualbox=false
+dmidecode | grep -q 'Product Name:.*VirtualBox' && virtualbox=true
+
 # if not virtualbox:
 # XXX add users and access
 # XXX uncomment wheel access in /etc/sudoers
 # XXX edit /etc/ssh/sshd_config
-
-# XXX kill firewall for now, fix it later?  Not needed anymore?
-#     /etc/init.d/iptables stop
 
 # setup centos
 yum -y groupremove "FCoE Storage Client"
@@ -35,8 +40,8 @@ useradd -m asterisk -s /bin/false
 # XXX install dyndns client if necessary?
 
 # install pyst
-cd /vagrant/src                 # XXX faster to do this in /tmp or something
-tar xvf pyst-0.6.50.tar.gz
+cd /tmp
+tar xvf /vagrant/src/pyst-0.6.50.tar.gz
 cd pyst-0.6.50
 python setup.py install --prefix=/usr/local
 ln -s /usr/local/lib/python2.6/site-packages/asterisk/ /usr/lib/python2.6/site-packages/
@@ -50,23 +55,31 @@ echo "su -s /bin/bash nobody -c '/usr/bin/festival --server &'" >> /etc/rc.d/rc.
 # build, install asterisk from source
 mkdir /opt/asterisk
 chown asterisk:asterisk /opt/asterisk
-cd /tmp # can't chown in /vagrant, and this is faster
+cd /tmp
 sudo -u asterisk tar xvf /vagrant/src/asterisk-11-current.tar.gz
 cd asterisk-11.5.1
 # XXX if 64bit, --libdir=/usr/lib64, but must be root to install?
 #     maybe we can put libdir within the prefix?
+if [ $virtualbox = true ]; then
+    # virtualbox on ubuntu thinkpad
+    # http://gentoo-what-did-you-say.blogspot.com/2011/07/finding-cpu-flags-using-gcc.html
+    CFLAGS=-march=core2
+fi
 sudo -u asterisk ./configure --prefix=/opt/asterisk --exec_prefix=/opt/asterisk
-# XXX if virtualbox, add CFLAGS=-march=core2
-# http://gentoo-what-did-you-say.blogspot.com/2011/07/finding-cpu-flags-using-gcc.html
+#CFLAGS=$CFLAGS
+
 # XXX we created these files with 'make menuselect' and quitting without
 #     selecting or deselecting anything.  We want to pare this down.
-# XXX may be broken, make menuselect if so
-cp /vagrant/src/menuselect.makeopts .
+if [ $virtualbox = true ]; then
+    # we set CFLAGS
+    cp /vagrant/src/menuselect.makeopts.virtualbox ./menuselect.makeopts
+else
+    cp /vagrant/src/menuselect.makeopts.ceres ./menuselect.makeopts
+fi
+
 chown asterisk:asterisk menuselect.makeopts
-cp /vagrant/src/menuselect.makedeps .
-chown asterisk:asterisk menuselect.makedeps
-# XXX if we configured with CFLAGS, edit menuselect.makeopts
-#MENUSELECT_CFLAGS=LOADABLE_MODULES 
+#cp /vagrant/src/menuselect.makedeps .
+#chown asterisk:asterisk menuselect.makedeps
 sudo -u asterisk make
 
 # XXX if we were 64 bit, we must be root to do this because of libdir,
@@ -77,11 +90,10 @@ sudo -u asterisk make samples
 make config # as root
 # XXX make install-logrotate?
 # this adds ASTARGS="-U asterisk"
-# XXX make sure this works
 cp /vagrant/src/safe_asterisk /opt/asterisk/sbin
 chown asterisk:asterisk /opt/asterisk/sbin/safe_asterisk
 
-# add the git host key
+# add the git host key so we can clone
 mkdir /home/asterisk/.ssh
 chown asterisk:asterisk /home/asterisk/.ssh
 cat /vagrant/src/known_hosts | sudo -u asterisk tee -a /home/asterisk/.ssh/known_hosts
@@ -90,13 +102,13 @@ chmod go-rwx /home/asterisk/.ssh
 # clone the git repos into the asterisk tree
 cd /opt/asterisk
 rm -rf etc/asterisk
-sudo -u asterisk git clone https://github.com/kra/futel-ceres-opt-asterisk-asterisk.git asterisk
+sudo -u asterisk git clone https://github.com/kra/futel-ceres-opt-asterisk-asterisk.git etc/asterisk
 cd /opt/asterisk/var/lib/asterisk
 rm -rf agi-bin
 sudo -u asterisk git clone https://github.com/lboom/futel-ceres-opt-asterisk-var-lib-asterisk-agi-bin.git agi-bin
 
 # copy vm_futel_users.inc template
-cat /vagrant/src/vm_futel_users.inc | sudo -u asterisk tee -a /opt/asterisk/etc/asterisk/vm_futel_users.inc
+cat /vagrant/src/vm_futel_users.inc | sudo -u asterisk tee /opt/asterisk/etc/asterisk/vm_futel_users.inc
 # XXX this has an XXXX password for the user in there, can we just keep that
 #     and make it a dummy mbox?  Else edit password.
 
@@ -114,7 +126,7 @@ cat /vagrant/src/vm_futel_users.inc | sudo -u asterisk tee -a /opt/asterisk/etc/
 #     secrets should refer to an /opt/futel/etc conf file for easier setup
 
 # XXX sigh, this can be made unnecessary
-find . -exec chown asterisk:asterisk {} \;
+find /opt/asterisk -exec chown asterisk:asterisk {} \;
 
 # XXX will need more ports for iax2 later - asterisk to asterisk
 /vagrant/src/iptables.sh
@@ -122,15 +134,20 @@ find . -exec chown asterisk:asterisk {} \;
 #     iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 service iptables save
 
+# XXX kill firewall for now because we didn't update the ssh port
+/etc/init.d/iptables stop
+
 # setup backups
 adduser backup
 usermod -a -G asterisk backup
 sudo -u backup mkdir /home/backup/.ssh
 sudo -u backup chmod go-rx /home/backup/.ssh
+# if not a devbox:
 # XXX copy backup key to backup's ~/.ssh/authorized_keys
 # XXX would be better to make backup's shell rsync or something
 # XXX backup user can't see /var/log/messages, /etc, /home
 
 # XXX logwatch
 
-# XXX restart asterisk, or reboot
+service asterisk stop
+service asterisk start
