2d48a9c8da67d8658d8a679b8a0c1f6d4af171ed
Karl Anderson <kra@monkey.org>
2014-04-26 20:44:14 -0700

    reorder

--- asteriskserver/src/iptables.sh
+++ asteriskserver/src/iptables.sh
@@ -5,25 +5,27 @@
 # clear all
 iptables -F
 
-# allow localhost
-iptables -A INPUT -i lo -j ACCEPT
-# allow from each asterisk box and client
-# XXX this gets resolved, want it to be hostname based
-# XXX should instead have a VPN!
-iptables -A INPUT --src futel2.dyndns.org -j ACCEPT
-# XXX cmon at least get a dyndns address
-iptables -A INPUT -s 67.171.136.104 -j ACCEPT
-# allow established connections
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-# let ssh in from anywhere
-# XXX would be better to have this on a different port
-#iptables -A INPUT -p tcp --dport 42422 -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
 # close everything
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
+
+# allow localhost
+iptables -A INPUT -i lo -j ACCEPT
+# allow established connections
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# allow from each asterisk box and client
+# XXX this gets resolved, want it to be hostname based
+# XXX should instead have a VPN!
+#iptables -A INPUT --src futel2.dyndns.org -j ACCEPT
+# XXX cmon at least get a dyndns address
+# XXX this only needs the asterisk ports
+iptables -A INPUT -s 67.160.151.198 -j ACCEPT
+# let ssh in from anywhere
+# XXX would be better to have this on a different port
+#iptables -A INPUT -p tcp --dport 42422 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
 # callcentric
 iptables -A INPUT -p udp -m udp -s 204.11.192.0/24 --dport 5060:5080 -j ACCEPT
