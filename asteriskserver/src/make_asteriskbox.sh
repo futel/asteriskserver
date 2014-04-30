ba9613b10f95b7197c09a6f03b54b3a99ba5d9a1
Karl Anderson <kra@monkey.org>
2014-04-29 18:02:57 -0700

    split out vitualbox, promote prod to stage

--- asteriskserver/src/make_asteriskbox.sh
+++ asteriskserver/src/make_asteriskbox.sh
@@ -4,11 +4,12 @@
 set -x
 
 do_ip=$1
+conf_version=$2
 
 scp -o StrictHostKeyChecking=no -i conf/id_rsa -r . futel@$do_ip:/tmp/vagrant
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo ln -s /tmp/vagrant /vagrant"
-ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo cp /vagrant/conf/sip_local.conf.prod /vagrant/conf/sip_local.conf"
-ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo cp /vagrant/conf/sip_callcentric.conf.prod /vagrant/conf/sip_callcentric.conf"
+ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo cp /vagrant/conf/sip_local.conf.$conf_version /vagrant/conf/sip_local.conf"
+ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo cp /vagrant/conf/sip_callcentric.conf.$conf_version /vagrant/conf/sip_callcentric.conf"
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo /vagrant/src/bootstrap.sh"
 # XXX need to service asterisk stop/start again?
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo rm -rf /tmp/src /tmp/conf"
