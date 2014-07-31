62cd93f88458a0ed39278db8dcb64e0713091a41
Karl Anderson <kra@monkey.org>
2014-07-30 19:27:47 -0700

    standardize bootstrap location

--- asteriskserver/src/centos6_32_baseconfig.sh
+++ asteriskserver/src/centos6_32_baseconfig.sh
@@ -4,25 +4,25 @@
 
 set -x
 
-/bin/cp -f /tmp/vagrant/src/sshd_config /etc/ssh/sshd_config
+/bin/cp -f /vagrant/src/sshd_config /etc/ssh/sshd_config
 service sshd restart
 
 # XXX will need more ports for iax2 later - asterisk to asterisk
 /etc/init.d/iptables stop
-/tmp/vagrant/src/iptables.sh
+/vagrant/src/iptables.sh
 service iptables save
 service iptables restart
 
 # add a futel user to log in as, and for vagrant commands
 useradd -m futel
 mkdir /home/futel/.ssh
-/bin/cp -f /tmp/vagrant/src/id_rsa.pub /home/futel/.ssh/authorized_keys
+/bin/cp -f /vagrant/src/id_rsa.pub /home/futel/.ssh/authorized_keys
 chown -R futel:futel /home/futel/.ssh
 chmod -R go-rwx /home/futel/.ssh
 usermod -a -G wheel futel
 
 # allow nopasswd sudo for futel user
-/bin/cp -f /tmp/vagrant/src/futel /etc/sudoers.d/futel
+/bin/cp -f /vagrant/src/futel /etc/sudoers.d/futel
 chmod go-rwx /etc/sudoers.d/futel
 
 # add non-root user for asterisk
@@ -32,7 +32,7 @@ useradd -m asterisk -s /bin/false
 adduser backup
 usermod -a -G asterisk backup
 mkdir /home/backup/.ssh
-cp /tmp/vagrant/src/backup_id_rsa.pub /home/backup/.ssh/authorized_keys
+cp /vagrant/src/backup_id_rsa.pub /home/backup/.ssh/authorized_keys
 chown -R backup:asterisk /home/backup/.ssh
 chmod -R go-rwx /home/backup/.ssh
 # add backup task to eurydice
