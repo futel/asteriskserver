d3c8bae315aac72028870a5a3a6a811a2c10b82a
Karl Anderson <kra@monkey.org>
2014-12-26 17:51:33 -0800

    fix improper creds

--- asteriskserver/src/make_baseconfig.sh
+++ asteriskserver/src/make_baseconfig.sh
@@ -6,7 +6,7 @@ set -x
 do_ip=$1
 
 scp -o StrictHostKeyChecking=no -r . root@$do_ip:/tmp/vagrant
-ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo ln -s /tmp/vagrant /vagrant"
+ssh -o StrictHostKeyChecking=no -t root@$do_ip "sudo ln -s /tmp/vagrant /vagrant"
 ssh -o StrictHostKeyChecking=no root@$do_ip /tmp/vagrant/src/centos6_32_baseconfig.sh
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo rm -rf /tmp/vagrant"
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo halt now"
