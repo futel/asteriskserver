f5d3f90c0b22b20cd883a7b99633aaba0bf6e7a0
Karl Anderson <kra@monkey.org>
2015-02-09 22:59:15 -0800

    basic metrics

--- asteriskserver/src/var-lib-asterisk-agi-bin/util.py
+++ asteriskserver/src/var-lib-asterisk-agi-bin/util.py
@@ -1,5 +1,6 @@
 #import datetime
 import sys, os, traceback
+import logging
 
 # directories for gsm files, in order of preference
 statement_dirs = [
@@ -8,6 +9,8 @@ statement_dirs = [
     '/opt/asterisk/var/lib/asterisk/sounds/futel/recordings/karl_bar/',
     '/opt/asterisk/var/lib/asterisk/sounds/en/'
     ]
+
+metric_filename = '/opt/asterisk/var/log/asterisk/metrics'
 
 def agi_tracebacker(agi_o, func, *args, **kwargs):
     try:
@@ -37,16 +40,36 @@ def say(agi_o, filename):
     # this seems to be parsed into args, punctuation may break it
     return agi_o.appexec('festival', filename)
 
-def format_log(name, val):
-    return "%s=%s" % (name, val)
+def metric(agi_o, name):
+    """ Create a metric event with name and values from agi_o. """
+    # A metric event is a key-value map, a plain metric is just a name and an
+    # optional arbitrary value and we should probably keep it simple. But we
+    # add a lot of default attributes to basically combine it with a verbose
+    # log.
+    items = dict(
+        (var, agi_o.get_variable(var))
+        for var in ('UNIQUEID', 'CHANNEL', 'CALLERID(number)'))
+    items['name'] = name
+    # writer is responsible for adding timestamp
+    metric_agilog(agi_o, **items)
+    metric_metriclog(**items)
 
-def metric(agi_o, message=None):
-    # if we're just logging, assume this is in there
-    #agi_o.verbose(str(datetime.datetime.now()), 1)
-    items = [(var, agi_o.get_variable(var))
-              for var in ('UNIQUEID', 'CHANNEL', 'CONTEXT', 'CALLERID(number)')]
-    if message:
-        items.append(('message', message))
-    line = ', '.join(format_log(name, value) for (name, value) in items)
+def metric_agilog(agi_o, **kwargs):
+    """ Log a formatted line to the asterisk log. """
+    line = ', '.join("%s=%s" % (k, v) for (k, v) in kwargs.items())
     # we only get verbose!
     agi_o.verbose(line, 1)
+
+def metric_metriclog(**kwargs):
+    """ Log a formatted line to the metric logfile. """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    ch = logging.FileHandler(metric_filename)
+    # write a line of comma separated key=value, should quote them!
+    logline = ', '.join("%s=%s" % (k, v) for (k, v) in kwargs.iteritems())
+    logline = ' '.join(('%(asctime)s', logline))
+    formatter = logging.Formatter(logline)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.info('')
+
