ddacf827d256d95f5c819378c928af53fff530e5
Karl Anderson <kra@monkey.org>
2015-02-11 21:17:09 -0800

    move to build dir

--- vpnbox/src/make_vpnbox.sh
+++ vpnbox/src/make_vpnbox.sh
@@ -6,6 +6,6 @@ set -x
 do_ip=$1
 
 scp -o StrictHostKeyChecking=no -r . root@$do_ip:/vagrant
-ssh -o StrictHostKeyChecking=no root@$do_ip /vagrant/src/vpnbox.sh
+ssh -o StrictHostKeyChecking=no root@$do_ip /vagrant/src/build/make_vpnbox.sh
 ssh -p42422 -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo rm -rf /vagrant"
 ssh -p42422 -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo halt now"
