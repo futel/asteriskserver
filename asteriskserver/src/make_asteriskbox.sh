eab211d38edf7021a88c5cad9f77a01c879d0812
Karl Anderson <kra@monkey.org>
2015-02-16 20:06:58 -0800

    Revert "delete source from correct location"
    This reverts commit b3fcfcf5c132f8e1f3bfd67d63a68cbd88206fa8.

--- asteriskserver/src/make_asteriskbox.sh
+++ asteriskserver/src/make_asteriskbox.sh
@@ -9,5 +9,5 @@ conf_version=$2
 scp -o StrictHostKeyChecking=no -i conf/id_rsa -r . futel@$do_ip:/tmp/vagrant
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo ln -s /tmp/vagrant /vagrant"
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo /vagrant/src/build/make_asteriskbox.sh $conf_version"
-ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo rm -rf /vagrant /tmp/vagrant"
+ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo rm -rf /tmp/src /tmp/conf"
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo halt now"
