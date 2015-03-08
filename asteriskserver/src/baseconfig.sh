b6c2ff5d77d59192638eae6901e2f321238a8224
Karl Anderson <kra@monkey.org>
2015-03-08 00:11:25 -0800

    normalize script names

--- asteriskserver/src/baseconfig.sh
+++ asteriskserver/src/baseconfig.sh
@@ -7,6 +7,6 @@ do_ip=$1
 
 scp -o StrictHostKeyChecking=no -r . root@$do_ip:/tmp/vagrant
 ssh -o StrictHostKeyChecking=no -t root@$do_ip "sudo ln -s /tmp/vagrant /vagrant"
-ssh -o StrictHostKeyChecking=no root@$do_ip /tmp/vagrant/src/centos6_32_baseconfig.sh
+ssh -o StrictHostKeyChecking=no root@$do_ip /vagrant/src/build/make_baseconfig.sh
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo rm -rf /tmp/vagrant"
 ssh -o StrictHostKeyChecking=no -t -i conf/id_rsa futel@$do_ip "sudo halt now"
