623c4988f4e539f5c16d497360bd4f5ebdff478d
Karl Anderson <kra@monkey.org>
2015-06-30 23:49:26 -0700

    first cut at community statements

---
+++ asteriskserver/src/var-lib-asterisk-agi-bin/community_ivr_statements.py
@@ -0,0 +1,10 @@
+statements = [
+    'for-the-futel-voice-conference',
+    'to-record-a-menu',
+    'for-audio-streams',
+    'for-the-message-board',
+    'welcome-to-the-Futel-message-board',
+    'please-leave-a-message',
+    'well-collect-them-and-play-them-back',
+    'if-you-want-a-confidential-reply-leave-your-voicemail-box-number',
+    'if-you-want-your-message-to-remain-private-say-so-and-we-wont-play-it-back-here']
