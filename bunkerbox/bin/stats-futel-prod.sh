eb2654f3ef4dff40780f4d88ce12dc1e4570877b
Karl Anderson <kra@monkey.org>
2016-01-10 21:50:06 -0800

    use bindir

--- bunkerbox/bin/stats-futel-prod.sh
+++ bunkerbox/bin/stats-futel-prod.sh
@@ -1,25 +1,15 @@
 #!/bin/sh                                                                       
 # make stats from backed up metrics
 
-# HOST=futel-prod.phu73l.net
-# DIRNAME=prod
-# REMOTEDIR=/opt/asterisk
-# KEYFILE=/opt/futel/ssh/id_rsa
-# SSHCMD="ssh -o StrictHostKeyChecking=no -i $KEYFILE -p 42422"
-# USER=backup
-# DATE=`date "+%Y-%m"`
-
-#PRODDIR=/opt/futel/backups/prod
-PRODDIR=/tmp/opt/futel/backups/prod
-#STATSDIR=/opt/futel/stats/prod
+BINDIR=/opt/futel/bin
+BACKUPSDIR=/tmp/opt/futel/backups/prod
 STATSDIR=/tmp/opt/futel/stats/prod
 
 # get latest metrics directory
-for lastdir in $PRODDIR/*; do
+for lastdir in $BACKUPSDIR/*; do
     true
 done
 
 metrics_filenames="$lastdir/opt/asterisk/var/log/asterisk/metrics $lastdir/opt/asterisk/var/log/asterisk/old/metrics*"
 
-# XXX make bindir
-./write_stats.py $metrics_filenames $STATSDIR
+$BINDIR/write_stats.py $metrics_filenames $STATSDIR
