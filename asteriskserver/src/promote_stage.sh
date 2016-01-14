533ec4d7a083fbcdc9b1b45f1e6f55bedbcacd86
lboom <tishbite@hobotech.org>
2016-01-13 18:12:34 -0800

    issue 45

--- asteriskserver/src/promote_stage.sh
+++ asteriskserver/src/promote_stage.sh
@@ -47,7 +47,7 @@ $SSH futel@$stage_ip "sudo chown -R asterisk:asterisk /tmp/stage"
 $SSH futel@$stage_ip "sudo -u asterisk cp /tmp/stage/vm_futel_users.inc /opt/asterisk/etc/asterisk/vm_futel_users.inc"
 $SSH futel@$stage_ip "sudo -u asterisk cp -r /tmp/stage/default /opt/asterisk/var/spool/asterisk/voicemail"
 # logs
-$SSH futel@$stage_ip "sudo -u asterisk cp -r /tmp/stage/asterisk /opt/asterisk/var/log/asterisk"
+$SSH futel@$stage_ip "sudo -u asterisk cp -r /tmp/stage/asterisk/* /opt/asterisk/var/log/asterisk"
 
 #$SSH futel@$stage_ip "sudo service asterisk start"
 $SSH futel@$stage_ip "sudo reboot"
