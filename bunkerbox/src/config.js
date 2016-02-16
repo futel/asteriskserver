2613ac124110461e135d6692c635ac1ad0346f59
Karl Anderson <kra@monkey.org>
2016-02-15 19:44:03 -0800

    split out config

---
+++ bunkerbox/src/config.js
@@ -0,0 +1,9 @@
+var config = {
+    channels: ["#cor", "#pdxbots"],
+    server: "irc.freenode.net",
+    botName: "mechaoperator",
+    userName: "mechaoperator",
+    realName: "Futel Mechanical Operator",
+};
+
+module.exports = { config: config};
