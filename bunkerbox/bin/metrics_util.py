64a4db9e2c8c36155dad69796bbdb272cba27b6b
Karl Anderson <kra@monkey.org>
2016-03-08 22:05:53 -0800

    collect milliseconds properly, store as text type to keep full datetime string

--- bunkerbox/bin/metrics_util.py
+++ bunkerbox/bin/metrics_util.py
@@ -4,6 +4,7 @@ metrics utils
 """
 import collections
 import datetime
+import time
 import itertools
 import json
 import operator
@@ -24,22 +25,27 @@ events_ignore = [
 def line_to_metric(line):
     """Return normalized dict from metric line."""
     # convert line of csv to list
-    # sigh, the first field is not quoted
-    (date, time, fields) = line.split(' ', 2)
-    metric = {'timestamp':" ".join((date, time))}
+    # sigh, the first field is not quoted eg '2010-09-06 22:38:15,292', and
+    # uses the old logging datetime format
+    (datestr, timestr, fields) = line.split(' ', 2)
     fields = fields.split(',')
     fields = [field.strip() for field in fields]
     # convert list of k=v to dict
     fields = [field.split('=') for field in fields]
-    metric.update(dict(fields))
+    metric = dict(fields)
     # normalize keys, deserialize values, add derived values
     # would be more rigorous to have a seprate table for derived values
     metric['callerid'] = metric.pop('CALLERID(number)')
     metric['uniqueid'] = metric.pop('UNIQUEID')
     metric['channel'] = metric.pop('CHANNEL')
     # deserialize values
-    metric['timestamp'] = datetime.datetime(
-        *map(int, re.split('[^\d]', metric['timestamp'])[:-1]))
+    (timestr, millistr) = timestr.split(',')
+    datestr = ' '.join((datestr, timestr))
+    timestamp = (
+        datetime.datetime.fromtimestamp(
+            time.mktime(time.strptime(datestr, '%Y-%m-%d %H:%M:%S'))) +
+        datetime.timedelta(milliseconds=int(millistr)))
+    metric['timestamp'] = timestamp
     # split ext from eg SIP/668-000002f1 SIP/callcentric-default-000002f3
     (_proto, extension) = metric['channel'].split('/')
     extension = '-'.join(extension.split('-')[:-1])
@@ -58,7 +64,7 @@ def write_metrics(metrics, db_path):
         c.execute(
             'INSERT INTO metrics '
             '(timestamp, callerid, uniqueid, channel, channel_extension, name) '
-            'VALUES (datetime(?),?,?,?,?,?)',
+            'VALUES (?,?,?,?,?,?)',
             (metric['timestamp'],
              metric['callerid'],
              metric['uniqueid'],
