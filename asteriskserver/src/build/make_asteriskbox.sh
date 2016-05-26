2655e66954e40502553f38a5e72dc7a91213dcef
Karl Anderson <kra@monkey.org>
2016-05-25 21:25:23 -0700

    do more provisioning in ansible

--- asteriskserver/src/build/make_asteriskbox.sh
+++ asteriskserver/src/build/make_asteriskbox.sh
@@ -12,22 +12,6 @@ conf_version=$1
 # virtualbox=false
 # dmidecode | grep -q 'Product Name:.*VirtualBox' && virtualbox=true
 
-#install libresample and jack
-pushd /tmp/
-tar xzvf /vagrant/src/libresample_0.1.3.orig.tar.gz
-pushd libresample-0.1.3
-./configure
-make
-popd
-
-pushd /tmp/
-tar xjvf /vagrant/src/jack-1.9.10.tar.bz2
-pushd jack-1.9.10
-./waf configure --alsa
-./waf build
-./waf install
-popd
-
 # build, install asterisk from source
 mkdir /opt/asterisk
 chown asterisk:asterisk /opt/asterisk
@@ -90,18 +74,6 @@ cp -f /vagrant/src/logwatch/conf/logwatch.conf /usr/share/logwatch/default.conf/
 # write the config files that are local or have secrets
 cp -f /vagrant/conf/logwatch.conf.$conf_version /etc/logwatch/conf/logwatch.conf
 
-# install mpg123 for mp3 playback
-# docs claim this is not needed but I am yet to see it work without
-cd /tmp
-tar xvf /vagrant/src/mpg123-1.22.2.tar.bz2
-cd mpg123-1.22.2
-./configure
-make && make install
-
-#make ldconf see locally installed libs
-echo "/usr/local/lib" > /etc/ld.so.conf.d/usr_local.conf
-/sbin/ldconfig
-
 # install asterisk event listener
 mkdir -p /opt/futel/src
 cp /vagrant/src/eventlistener.py /opt/futel/src
@@ -115,9 +87,6 @@ echo "su -s /bin/bash nobody -c /opt/futel/src/eventlistener.py" >> /etc/rc.d/rc
 
 # run rc.local now
 nohup /etc/rc.d/rc.local &
-
-# let's make pd for toorcamp/houseguest
-/vagrant/src/build/make_pd.sh
 
 # TODO this will be off by default for now. currently 240MB
 #	uncomment for testing or for DO build
