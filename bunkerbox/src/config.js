ec7447eec283fb7c2bf7b5887b1013e7b25fde5d
Karl Anderson <kra@monkey.org>
2016-07-02 20:43:36 -0700

    remove ctrlh

--- bunkerbox/src/config.js
+++ bunkerbox/src/config.js
@@ -1,5 +1,5 @@
 var config = {
-    channels: ["#cor", "#pdxbots", "#ctrlh"],
+    channels: ["#cor", "#pdxbots"],
     noisyChannels: ['#cor', '#pdxbots'],
     server: "irc.freenode.net",
     botName: "mechaoperator",
