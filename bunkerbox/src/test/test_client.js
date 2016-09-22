d08e5300efbe2bfab9f8a3c178b0b2eadddb7c9c
Karl Anderson <kra@monkey.org>
2016-09-21 18:36:48 -0700

    implement some unit tests for real

--- bunkerbox/src/test/test_client.js
+++ bunkerbox/src/test/test_client.js
@@ -1,8 +1,6 @@
+var assert = require('assert');
+var sinon = require('sinon');
 var client_mod = require('../client');
-
-// function Server() {};
-// Server.prototype.connect = function() { console.log("connect"); }
-// var server = Server();
 
 var getClient = function() {
     var client = new client_mod.Client('server', 'nick', {}, ['noisyChannel'], 'dbFileName');
@@ -11,73 +9,126 @@ var getClient = function() {
     // disable first throttle
     client.throttleDate = new Date() - 10 * 1000;
     // patch for testing
-    client.say = function(channel, text) {console.log(channel, text)};
+    sinon.spy(client, "say");
     return client;
 }
 
-var client = getClient();
+var testOneSay = function(client, to, message) {
+    assert(client.say.calledOnce);
+    assert.equal(client.say.args[0][0], to);
+    assert.equal(client.say.args[0][1], message);
+}
 
-// // add event listeners for testing
-// ['abort', 'action', 'connect', 'error', 'join', 'kill', 'message#', 'message', 'motd', 'names', 'names', 'nick', 'notice', 'opered', 'part', 'ping', 'pm', 'pong', 'quit', 'raw', 'registered', 'selfMessage'].forEach(
-//     function(element) {
-//         client.on(element, arguments => {
-//             console.log(element, arguments);
-//         });
-//     }
-// );
+var testSays = function(client, to, messages) {
+    assert.equal(client.say.args.length, messages.length);
+    client.say.args.forEach(function(arg) {
+        assert.equal(arg[0], to);
+        assert.equal(arg[1], messages.shift());
+    });
+}
 
-// test pm commands
-// from Use "help" for help.
-getClient().pm('from', 'xyzzy', 'message');
-// from Hi from!
-getClient().pm('from', 'hi', 'message');
-// from available commands:
-// from hi say hello
-// from help get command help
-// from latest [extension [extension...]] get latest events
-// from stats [days [extension]] get event stats
-// from recentbad get recent events
-getClient().pm('from', 'help', 'message');
+describe('main', function() {
+    var client = null;
 
-// test channel bang commands
-//
-getClient().channelMessage('from', 'to', '!xyzzy', 'message');
-// to Hi from!
-getClient().channelMessage('from', 'to', '!hi', 'message');
-// 
-getClient().channelMessage('from', 'to', '!help', 'message');
+    beforeEach(function() {
+        client = getClient();
+    });
 
-// test channel talk commands addressed to nick
-//
-getClient().channelMessage('from', 'to', 'nick: xyzzy', {args: []});
-// to Hi from!
-getClient().channelMessage('from', 'to', 'nick: hi', {args: []});
-// 
-getClient().channelMessage('from', 'to', 'nick: help', {args: []});
+    describe('pm', function() {
+        describe('unknown', function() {
+            it('should pm help summary from unknown input', function() {
+                client.pm('from', 'xyzzy', 'message');
+                testOneSay(client, 'from', 'Use "help" for help.');
+            });
+        });
+        describe('hi', function() {
+            it('should pm hi response', function() {
+                client.pm('from', 'hi', 'message');
+                testOneSay(client, 'from', 'Hi from!');
+            });
+        });
+        describe('help', function() {
+            it('should pm help response', function() {
+                client.pm('from', 'help', 'message');
+                testSays(client, 'from', [
+                    'available commands:',
+                    'hi say hello',
+                    'help get command help',
+                    'latest [extension [extension...]] get latest events',
+                    'stats [days [extension]] get event stats',
+                    'recentbad get recent events'])
+            });
+        });
+    });
 
-// test channel talk responses
-//
-getClient().channelMessage('from', 'to', 'nick is foo', {args: []});
-//
-getClient().channelMessage('from', 'to', 'xyzzy', {args: ['noisyChannel']});
-//
-getClient().channelMessage('from', 'to', 'hi', {args: ['noisyChannel']});
-//
-getClient().channelMessage('from', 'to', 'help', {args: ['noisyChannel']});
-// to No, from, you're foo!
-getClient().channelMessage('from', 'to', 'nick is foo', {args: ['noisyChannel']});
-// to No, from, you're bar!
-getClient().channelMessage('from', 'to', 'foo nick is bar', {args: ['noisyChannel']});
-// to Suddenly someone'll say, like, plate, or shrimp, or plate o' shrimp out of the blue, no explanation.
-getClient().channelMessage('from', 'to', 'foo plate bar', {args: ['noisyChannel']});
-// to Yes.
-getClient().channelMessage('from', 'to', 'no', {args: ['noisyChannel']});
-// three says of greeting sayings
-getClient().channelMessage('from', 'to', 'foo nick bar', {args: ['noisyChannel']});
-getClient().channelMessage('from', 'to', 'foo nick bar', {args: ['noisyChannel']});
-getClient().channelMessage('from', 'to', 'foo nick bar', {args: ['noisyChannel']});
-client = getClient();
-// to No, from, you're bar!
-client.channelMessage('from', 'to', 'foo nick is bar', {args: ['noisyChannel']});
-//
-client.channelMessage('from', 'to', 'foo nick is bar', {args: ['noisyChannel']});
+    describe('bang commands in channel', function() {
+        describe('unknown', function() {
+            it('should not respond to unknown command', function() {
+                client.channelMessage('from', 'to', '!xyzzy', 'message');
+                assert.equal(false, client.say.called);
+            });
+        });
+        describe('hi', function() {
+            it('should say hi response', function() {
+                client.channelMessage('from', 'to', '!hi', 'message');
+                testOneSay(client, 'to', 'Hi from!');                
+            });
+        });
+        describe('help', function() {
+            it('should not respond to help command', function() {
+                client.channelMessage('from', 'to', '!help', 'message');
+                assert.equal(false, client.say.called);
+            });
+        });
+    });
+
+    describe('nick hails in channel', function() {
+        describe('unknown', function() {
+            it('should not respond to unknown hail', function() {
+                client.channelMessage('from', 'to', 'nick: xyzzy', 'message');
+                assert.equal(false, client.say.called);
+            });
+        });
+        describe('hi', function() {
+            it('should say hi response', function() {
+                client.channelMessage('from', 'to', 'nick: hi', 'message');
+                testOneSay(client, 'to', 'Hi from!');                
+            });
+        });
+        describe('help', function() {
+            it('should not respond to help command', function() {
+                client.channelMessage('from', 'to', 'nick: help', 'message');
+                assert.equal(false, client.say.called);
+            });
+        });
+    });
+   
+    
+});
+
+// // test channel talk responses
+// //
+// getClient().channelMessage('from', 'to', 'nick is foo', {args: []});
+// //
+// getClient().channelMessage('from', 'to', 'xyzzy', {args: ['noisyChannel']});
+// //
+// getClient().channelMessage('from', 'to', 'hi', {args: ['noisyChannel']});
+// //
+// getClient().channelMessage('from', 'to', 'help', {args: ['noisyChannel']});
+// // to No, from, you're foo!
+// getClient().channelMessage('from', 'to', 'nick is foo', {args: ['noisyChannel']});
+// // to No, from, you're bar!
+// getClient().channelMessage('from', 'to', 'foo nick is bar', {args: ['noisyChannel']});
+// // to Suddenly someone'll say, like, plate, or shrimp, or plate o' shrimp out of the blue, no explanation.
+// getClient().channelMessage('from', 'to', 'foo plate bar', {args: ['noisyChannel']});
+// // to Yes.
+// getClient().channelMessage('from', 'to', 'no', {args: ['noisyChannel']});
+// // three says of greeting sayings
+// getClient().channelMessage('from', 'to', 'foo nick bar', {args: ['noisyChannel']});
+// getClient().channelMessage('from', 'to', 'foo nick bar', {args: ['noisyChannel']});
+// getClient().channelMessage('from', 'to', 'foo nick bar', {args: ['noisyChannel']});
+// var client = getClient();
+// // to No, from, you're bar!
+// client.channelMessage('from', 'to', 'foo nick is bar', {args: ['noisyChannel']});
+// //
+// client.channelMessage('from', 'to', 'foo nick is bar', {args: ['noisyChannel']});
