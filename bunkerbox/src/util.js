b645e10ba72fcf57dc34950d32947a22340d85bb
Karl Anderson <kra@monkey.org>
2016-11-20 17:07:20 -0800

    whoops, add util

---
+++ bunkerbox/src/util.js
@@ -0,0 +1,12 @@
+function extend(destination, source) {
+  for (var k in source) {
+    if (source.hasOwnProperty(k)) {
+      destination[k] = source[k];
+    }
+  }
+  return destination; 
+}
+
+module.exports = {
+    extend: extend
+};
