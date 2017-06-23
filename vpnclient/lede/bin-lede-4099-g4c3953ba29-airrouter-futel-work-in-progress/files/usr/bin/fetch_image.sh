79349d9cae2f442a97238e14c5f54ef2d7d179d0
Karl Anderson <kra@monkey.org>
2017-06-22 21:24:32 -0700

    moved to futel-vpnclient

--- vpnclient/lede/bin-lede-4099-g4c3953ba29-airrouter-futel-work-in-progress/files/usr/bin/fetch_image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /tmp
-scp openwrt@hawg:src/lede/bin/targets/ar71xx/generic/lede-ar71xx-generic-ubnt-airrouter-squashfs-sysupgrade.bin /tmp/
--- vpnclient/lede/bin-lede-4099-g4c3953ba29-airrouter-futel-work-in-progress/files/usr/bin/since-last-flash.sh
+++
@@ -1,1 +0,0 @@
-uci show | diff -u /etc/uci-firstboot - ; for f in $(find /overlay/upper -type f | sed 's|/overlay/upper/||' | grep -v etc/config | grep -v etc/uci-firstboot) ; do diff -uN /rom/$f /$f ; done
