8289ec15de475a8686303f292b0b32c3a1da40f7
Alex Norman <alex@x37v.info>
2018-08-19 18:39:54 -0700

    can create normalized assets

--- asteriskserver/scripts/update_audio_assets.rb
+++ asteriskserver/scripts/update_audio_assets.rb
@@ -1,26 +1,86 @@
 #!/usr/bin/env ruby
 
+#https://wiki.asterisk.org/wiki/display/AST/Asterisk+10+Codecs+and+Audio+Formats
+
 require 'find'
 require 'fileutils'
+require 'sndfile'
 
 SRC_DIR = ARGV[1]
 DEST_DIR = ARGV[0]
 
 puts "updating #{DEST_DIR} with audio from #{SRC_DIR}"
 
+sln_ext = {
+  8_000 => "sln",
+  12_000 => "sln12",
+  16_000 => "sln16",
+  24_000 => "sln24",
+  32_000 => "sln32",
+  44_100 => "sln44",
+  48_000 => "sln48",
+  96_000 => "sln96",
+  192_000 =>"sln192"
+}
+
+known_ext = sln_ext.values + ["mp3", "gsm"]
+
 remove = SRC_DIR.split("/").length
 Find.find(SRC_DIR).each do |f|
+  cleanup = []
   next if File.directory?(f)
 
   src = f
+  src_ext = File.extname(src)
 
   #create the destination
   f = f.split("/").pop(remove).join("/")
   dst = File.join(DEST_DIR, f)
+  base = File.join(File.dirname(dst), File.basename(dst, ".*"))
 
-  next if File.exist?(dst)
-  puts "#{src} -> #{dst}"
+  #simply copy mp3, if the source is newer
+  if src_ext == ".mp3"
+    unless File.exist?(dst) and File.mtime(dst) > File.mtime(src)
+      FileUtils.mkdir_p(File.dirname(dst))
+      FileUtils.copy(src, dst)
+    end
+    next
+  end
+
+  info = Sndfile::File.info(src)
+  out_ext = sln_ext[info.samplerate]
+  raise "#{src} has an unsuppored sampling rate of #{info.samplerate}" unless out_ext
+
+  dst = base + "." + out_ext
+  #ditch if the file exists and the dest is newer
+  next if File.exist?(dst) and File.mtime(dst) > File.mtime(src)
+
+  #make the dir
   FileUtils.mkdir_p(File.dirname(dst))
-  FileUtils.copy_file(src, dst)
+
+  #report
+  puts "#{src} -> #{dst}"
+
+  #convert gsm to wav so we can normalize
+  if src_ext == ".gsm"
+    wav = base + "-tmp.wav"
+    raise "cannot create tmp wav from gsm" unless system("sndfile-convert", "-pcm16", src, wav)
+    cleanup << wav
+    src = wav
+  end
+
+  #make mono
+  if info.channels != 1
+    mono = base + "-tmp-mono.wav"
+    raise "failed to make a mono file of #{src}" unless system("sndfile-mix-to-mono", src, mono)
+    cleanup << mono
+    src = mono
+  end
+
+  #copy then normalize
+  FileUtils.copy(src, dst)
+  raise "failed to normalize" unless system("normalize-audio", "--quiet", "--peak", dst)
+
+  cleanup.each { |f| File.unlink(f) }
 end
 
