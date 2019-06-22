33b7691e728d44a9cac3b5b40afc4740807a797d
Karl Anderson <kra@monkey.org>
2019-06-21 22:16:38 -0700

    add sln48 audio format

--- asteriskserver/src/var/lib/asterisk/agi-bin/util.py
+++ asteriskserver/src/var/lib/asterisk/agi-bin/util.py
@@ -57,7 +57,7 @@ def sound_path(sound_name, preferred_subs=None):
     for statement_dir in dirs:
         path = statement_dir + sound_name
         # stream_file and Background want it without the extension
-        suffixes = ['.gsm', '.sl44', '.sln', '.sln44', '.wav']
+        suffixes = ['.gsm', '.sl44', '.sln', '.sln44', '.sln48', '.wav']
         for suffix in suffixes:
             if os.path.isfile(path + suffix):
                 return path
