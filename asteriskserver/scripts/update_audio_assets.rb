2d48630cce41ee31732113cf9e3d60799b8ba253
Karl Anderson <kra@monkey.org>
2019-07-26 21:28:08 -0700

    use update_audio_assets.rb from xnor's compress branch, add requirements

--- asteriskserver/scripts/update_audio_assets.rb
+++ asteriskserver/scripts/update_audio_assets.rb
@@ -10,6 +10,8 @@ require 'find'
 require 'fileutils'
 require 'sndfile'
 
+SOX_COMPAND = "compand 0.14,1 6:-70,-24,-4.8 -8 -90 0.2"
+
 SRC_DIR = ARGV[1]
 DEST_DIR = ARGV[0]
 
@@ -29,7 +31,10 @@ sln_ext = {
 
 known_ext = sln_ext.values + ["mp3", "gsm"]
 
+script_mtime = File.mtime(__FILE__)
+
 Find.find(SRC_DIR).each do |f|
+  is_raw = false
   cleanup = []
   next if File.directory?(f)
 
@@ -48,12 +53,13 @@ Find.find(SRC_DIR).each do |f|
   else
     info = Sndfile::File.info(src)    
     out_ext = sln_ext[info.samplerate]
+    is_raw = true
     raise "#{src} has an unsuppored sampling rate of #{info.samplerate}" unless out_ext
   end
 
   dst = base + "." + out_ext
   #ditch if the file exists and the dest is newer
-  next if File.exist?(dst) and File.mtime(dst) > File.mtime(src)
+  next if File.exist?(dst) and File.mtime(dst) > File.mtime(src) and File.mtime(dst) > script_mtime
 
   #make the dir
   FileUtils.mkdir_p(File.dirname(dst))
@@ -61,13 +67,11 @@ Find.find(SRC_DIR).each do |f|
   #report
   puts "#{src} -> #{dst}"
 
-  #convert gsm to wav so we can normalize
-  if src_ext == ".gsm"
-    wav = base + "-tmp.wav"
-    raise "cannot create tmp wav from gsm" unless system("sndfile-convert", "-pcm16", src, wav)
-    cleanup << wav
-    src = wav
-  end
+  #convert to wav
+  wav = base + "-tmp.wav"
+  raise "cannot create tmp wav from #{out_ext}" unless system("sox", src, wav)
+  cleanup << wav
+  src = wav
 
   if src_ext != ".mp3"  
     #make mono
@@ -79,10 +83,39 @@ Find.find(SRC_DIR).each do |f|
     end
   end
 
-  #copy then normalize
-  FileUtils.copy(src, dst)
-  raise "failed to normalize" unless system("normalize-audio", "--quiet", "--peak", dst)
+  #compress
+  compressed = base + "-tmp-compressed.wav"
+  raise "failed to apply dynamic range compression" unless system("sox #{src} #{compressed} #{SOX_COMPAND}")
+  cleanup << compressed
+  src = compressed
 
+  #convert back to mp3
+  if src_ext == ".mp3"  
+    mp3 = base + "-tmp-mp3.mp3"
+    raise "failed to encode to mp3" unless system("lame", "--quiet", src, mp3)
+    src = mp3
+    cleanup << mp3
+  end
+
+  #copy then normalize
+  normalized = base + "-tmp-norm." + out_ext
+  cleanup << normalized
+  FileUtils.copy(src, normalized) #normalize happens in place, so copy the file first
+  raise "failed to normalize" unless system("normalize-audio", "--quiet", "--peak", normalized)
+  src = normalized
+
+  #remove header if it is raw
+  if is_raw
+    raw = base + "-tmp.raw"
+    raise "cannot create raw encoding" unless system("sndfile-convert", "-pcm16", src, raw)
+    cleanup << raw
+    src = raw
+  end
+
+  #copy to the final location
+  FileUtils.copy(src, dst)
+
+  #cleanup
   cleanup.each { |f| File.unlink(f) }
 end
 
