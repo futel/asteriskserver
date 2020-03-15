c1ba56ca2493bdb2dbdb496d6c13c21390ad2ed9
Karl Anderson <kra@monkey.org>
2020-03-15 14:14:06 -0700

    move members and admin to lua

--- asteriskserver/src/etc/asterisk/extensions.lua
+++ asteriskserver/src/etc/asterisk/extensions.lua
@@ -1,5 +1,6 @@
 package.path = package.path .. ';/opt/asterisk/etc/asterisk/?.lua'
 require("util")
+require("extensions_admin")
 require("extensions_peopleshomes")
 require("extensions_conversations")
 require("extensions_missedconnections")
@@ -10,6 +11,7 @@ extensions = {
     };
 }
 
+for k,v in pairs(extensions_admin) do extensions[k] = v end
 for k,v in pairs(extensions_peopleshomes) do extensions[k] = v end
 for k,v in pairs(extensions_conversations) do extensions[k] = v end
 for k,v in pairs(extensions_missedconnections) do extensions[k] = v end
---
+++ asteriskserver/src/etc/asterisk/extensions_admin.lua
@@ -0,0 +1,58 @@
+function menu_admin_main(context, extension)
+    return menu(
+        {"fewtel",
+        "for-the-fewtel-voice-conference",
+        "press-one",
+        "for",
+        "the",
+        "outgoing",
+        "menus",
+        "press-two",
+        "for-an-internal-dialtone",
+        "press-three",
+        "to-record-a-menu",
+        "press-four",
+        "for-the-operator",
+        "press-zero"},
+    "",
+    context,
+    extension)
+end
+
+function menu_member_main(context, extension)
+    return menu(
+        {"fewtel",
+        "for-the-fewtel-voice-conference",
+        "press-one",
+        "for-an-internal-dialtone",
+        "press-two",
+        "to-record-a-menu",
+        "press-three",
+        "for-the-wildcard-line",
+        "press-four",
+        "for-the-operator",
+        "press-zero"},
+    "",
+    context,
+    extension)
+end
+
+extensions_admin = {
+    admin_main = context(
+        menu_admin_main,
+        "admin_main",
+        {"futel-conf",
+         "outgoing-chooser",
+         "internal-dialtone-wrapper",
+         "record",
+         "operator"});
+    member_main = context(
+        menu_member_main,
+        "member_main",
+        {"futel-conf",
+         "internal-dialtone-wrapper",
+         "record",
+         "wildcard-line-outgoing",
+         "operator"});
+         
+}
