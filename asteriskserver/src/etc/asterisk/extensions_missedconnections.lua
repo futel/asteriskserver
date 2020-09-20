52e7a3f40e3122483d48b9d7be9b2790a6656cf5
Karl Anderson <kra@monkey.org>
2020-09-19 22:12:44 -0700

    add new content

--- asteriskserver/src/etc/asterisk/extensions_missedconnections.lua
+++ asteriskserver/src/etc/asterisk/extensions_missedconnections.lua
@@ -4,6 +4,11 @@ MAILBOX_MAIN=1500 -- mailbox to record a new missed connection
 MAILBOX_ONE=1501  -- maibox to record a response to content 1
 MAILBOX_TWO=1502  -- maibox to record a response to content 2
 MAILBOX_THREE=1503  -- maibox to record a response to content 3
+MAILBOX_FOUR=1504  -- maibox to record a response to content 3
+MAILBOX_FIVE=1505  -- maibox to record a response to content 3
+MAILBOX_SIX=1506  -- maibox to record a response to content 3
+MAILBOX_SEVEN=1507  -- maibox to record a response to content 3
+MAILBOX_EIGHT=1508  -- maibox to record a response to content 3
 
 function menu_message_record(context, extension)
     util.say("your-recording-can-last-up-to-two-minutes", "missed-connections")
@@ -34,6 +39,46 @@ function menu_message_three_response_record(context, extension)
     util.say("any-listener-can-hear-responses-to-this-missed-connection", "missed-connections")
     util.say("record-your-message-after-the-tone", "missed-connections")        
     app.VoiceMail(MAILBOX_THREE, "s")
+    app.Hangup()
+end
+
+function menu_message_four_response_record(context, extension)
+    util.say("your-response-can-last-up-to-2-minutes", "missed-connections")
+    util.say("any-listener-can-hear-responses-to-this-missed-connection", "missed-connections")
+    util.say("record-your-message-after-the-tone", "missed-connections")        
+    app.VoiceMail(MAILBOX_FOUR, "s")
+    app.Hangup()
+end
+
+function menu_message_five_response_record(context, extension)
+    util.say("your-response-can-last-up-to-2-minutes", "missed-connections")
+    util.say("any-listener-can-hear-responses-to-this-missed-connection", "missed-connections")
+    util.say("record-your-message-after-the-tone", "missed-connections")        
+    app.VoiceMail(MAILBOX_FIVE, "s")
+    app.Hangup()
+end
+
+function menu_message_six_response_record(context, extension)
+    util.say("your-response-can-last-up-to-2-minutes", "missed-connections")
+    util.say("any-listener-can-hear-responses-to-this-missed-connection", "missed-connections")
+    util.say("record-your-message-after-the-tone", "missed-connections")        
+    app.VoiceMail(MAILBOX_SIX, "s")
+    app.Hangup()
+end
+
+function menu_message_seven_response_record(context, extension)
+    util.say("your-response-can-last-up-to-2-minutes", "missed-connections")
+    util.say("any-listener-can-hear-responses-to-this-missed-connection", "missed-connections")
+    util.say("record-your-message-after-the-tone", "missed-connections")        
+    app.VoiceMail(MAILBOX_SEVEN, "s")
+    app.Hangup()
+end
+
+function menu_message_eight_response_record(context, extension)
+    util.say("your-response-can-last-up-to-2-minutes", "missed-connections")
+    util.say("any-listener-can-hear-responses-to-this-missed-connection", "missed-connections")
+    util.say("record-your-message-after-the-tone", "missed-connections")        
+    app.VoiceMail(MAILBOX_EIGHT, "s")
     app.Hangup()
 end
 
@@ -85,6 +130,11 @@ extensions = {
              [1]={{"/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/clip_12_29_19", "to-hear-more-and-reply"}, "message_one_play"},
              [2]={{"/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/clip_01_03_20", "to-hear-more-and-reply"}, "message_two_play"},
              [3]={{"/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/clip_01_06_20", "to-hear-more-and-reply"}, "message_three_play"},
+             [4]={{"/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0000-clip", "to-hear-more-and-reply"}, "message_four_play"},
+             [5]={{"/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0001-clip", "to-hear-more-and-reply"}, "message_five_play"},
+             [6]={{"/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0002-clip", "to-hear-more-and-reply"}, "message_six_play"},
+             [7]={{"/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0003-clip", "to-hear-more-and-reply"}, "message_seven_play"},
+             [8]={{"/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0004-clip", "to-hear-more-and-reply"}, "message_eight_play"},
              [9]={"to-record-a-missed-connection", "message_record"}},
          statement_dir="missed-connections"}),
     message_record = util.context_array(menu_message_record, {}),
@@ -129,6 +179,52 @@ extensions = {
     --     {intro_statements={"message-three-response-content"},
     --      menu_entries={{"to-respond-to-this-message-with-a-recording", "message_three_response_record"})
     --    statement_dir="missed-connections",
+    message_four_play = util.context(
+        {intro_statements={
+             "/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0000"},
+         menu_entries={
+             {"to-respond-to-this-message-with-a-recording",
+              "message_four_response_record"}},
+         statement_dir="missed-connections"}),
+    message_four_response_record = util.context_array(
+        menu_message_four_response_record, {}),
+    message_five_play = util.context(
+        {intro_statements={
+             "/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0001"},
+         menu_entries={
+             {"to-respond-to-this-message-with-a-recording",
+              "message_five_response_record"}},
+         statement_dir="missed-connections"}),
+    message_five_response_record = util.context_array(
+        menu_message_five_response_record, {}),
+    message_six_play = util.context(
+        {intro_statements={
+             "/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0002"},
+         menu_entries={
+             {"to-respond-to-this-message-with-a-recording",
+              "message_six_response_record"}},
+         statement_dir="missed-connections"}),
+    message_six_response_record = util.context_array(
+        menu_message_six_response_record, {}),
+    message_seven_play = util.context(
+        {intro_statements={
+             "/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0003"},
+         menu_entries={
+             {"to-respond-to-this-message-with-a-recording",
+              "message_seven_response_record"}},
+         statement_dir="missed-connections"}),
+    message_seven_response_record = util.context_array(
+        menu_message_seven_response_record, {}),
+    message_eight_play = util.context(
+        {intro_statements={
+             "/opt/asterisk/var/lib/asterisk/sounds/futel/missed-connections/msg0004"},
+         menu_entries={
+             {"to-respond-to-this-message-with-a-recording",
+              "message_eight_response_record"}},
+         statement_dir="missed-connections"}),
+    message_eight_response_record = util.context_array(
+        menu_message_eight_response_record, {}),
+    
 }
 
 return extensions
