8416aaf91a430acd743d3e396c9b7903274bb8c9
Karl Anderson <kra@monkey.org>
2020-12-21 14:47:22 -0800

    move new oskar extension to 530, not 525

--- asteriskserver/src/etc/asterisk/extensions.lua
+++ asteriskserver/src/etc/asterisk/extensions.lua
@@ -2,10 +2,8 @@ package.path = package.path .. ';/opt/asterisk/etc/asterisk/?.lua'
 
 extensions_admin = require("extensions_admin")
 extensions_challenge = require("extensions_challenge")
-extensions_conversations = require("extensions_conversations")
 extensions_holdthephone = require("extensions_holdthephone")
 extensions_outgoing = require("extensions_outgoing")
-extensions_peopleshomes = require("extensions_peopleshomes")
 extensions_wildcard_line = require("extensions_wildcard_line")
 
 extensions = {
