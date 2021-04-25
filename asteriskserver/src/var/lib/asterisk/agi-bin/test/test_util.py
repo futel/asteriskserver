c0f7de1b0bdc961cf792a55215132a1b0068872e
Karl Anderson <kra@monkey.org>
2021-04-24 21:29:41 -0700

    update integer literals for python 3

--- asteriskserver/src/var/lib/asterisk/agi-bin/test/test_util.py
+++ asteriskserver/src/var/lib/asterisk/agi-bin/test/test_util.py
@@ -18,59 +18,59 @@ class TestUtil(unittest.TestCase):
 
     def test_within_timestrs_hour_minute(self):
         assert not util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2016, 01, 01, 00, 30))
+            "01:00", "02:00", datetime.datetime(2016, 1, 1, 0, 30))
         assert util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2016, 01, 01, 01, 00))
+            "01:00", "02:00", datetime.datetime(2016, 1, 1, 1, 0))
         assert util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2016, 01, 01, 01, 30))
+            "01:00", "02:00", datetime.datetime(2016, 1, 1, 1, 30))
         assert not util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2016, 01, 01, 02, 00))
+            "01:00", "02:00", datetime.datetime(2016, 1, 1, 2, 0))
         assert not util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2016, 01, 01, 02, 30))
+            "01:00", "02:00", datetime.datetime(2016, 1, 1, 2, 30))
         assert not util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2017, 02, 02, 00, 30))
+            "01:00", "02:00", datetime.datetime(2017, 2, 2, 0, 30))
         assert util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2017, 02, 02, 01, 00))
+            "01:00", "02:00", datetime.datetime(2017, 2, 2, 1, 0))
         assert util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2017, 02, 02, 01, 30))
+            "01:00", "02:00", datetime.datetime(2017, 2, 2, 1, 30))
         assert not util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2017, 02, 02, 02, 00))
+            "01:00", "02:00", datetime.datetime(2017, 2, 2, 2, 0))
         assert not util.within_timestrs(
-            "01:00", "02:00", datetime.datetime(2017, 02, 02, 02, 30))
+            "01:00", "02:00", datetime.datetime(2017, 2, 2, 2, 30))
 
     def test_within_timestrs_iso(self):
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 01, 01, 00, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 1, 1, 0, 30)))
         self.assertTrue(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 01, 01, 01, 00)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 1, 1, 1, 0)))
         self.assertTrue(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 01, 01, 01, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 1, 1, 1, 30)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 01, 01, 02, 00)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 1, 1, 2, 0)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 01, 01, 02, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2016, 1, 1, 2, 30)))
 
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 01, 01, 00, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 1, 1, 0, 30)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 01, 01, 01, 00)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 1, 1, 1, 0)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 01, 01, 01, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 1, 1, 1, 30)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 01, 01, 02, 00)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 1, 1, 2, 0)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 01, 01, 02, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2017, 1, 1, 2, 30)))
 
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 01, 01, 00, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 1, 1, 0, 30)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 01, 01, 01, 00)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 1, 1, 1, 0)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 01, 01, 01, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 1, 1, 1, 30)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 01, 01, 02, 00)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 1, 1, 2, 0)))
         self.assertFalse(util.within_timestrs(
-            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 01, 01, 02, 30)))
+            "2016-01-01T1:00", "2016-01-01T2:00", datetime.datetime(2015, 1, 1, 2, 30)))
 
     def test_relevant_config(self):
         assert util.relevant_config(
@@ -80,7 +80,7 @@ class TestUtil(unittest.TestCase):
               'start_time': '01:00',
               'end_time': '02:00'}],
             '666',
-            datetime.datetime(2016, 01, 01, 00, 30)) is None
+            datetime.datetime(2016, 1, 1, 0, 30)) is None
         self.assertEqual(
             util.relevant_config(
                 [{'extensions':[666],
@@ -88,7 +88,7 @@ class TestUtil(unittest.TestCase):
                   'end_time': '02:00',
                   'action': 'bar'}],
                 '666',
-                datetime.datetime(2016, 01, 01, 01, 30)),
+                datetime.datetime(2016, 1, 1, 1, 30)),
             {'extensions':[666],
              'start_time': '01:00',
              'end_time': '02:00',
@@ -100,7 +100,7 @@ class TestUtil(unittest.TestCase):
                   'end_time': '2016-01-01T02:00',
                   'action': 'bar'}],
                 '666',
-                datetime.datetime(2016, 01, 01, 01, 30)),
+                datetime.datetime(2016, 1, 1, 1, 30)),
             {'extensions':[666],
              'start_time': '2016-01-01T01:00',
              'end_time': '2016-01-01T02:00',
