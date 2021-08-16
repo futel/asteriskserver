c435eacb1999ed7d41bb354a1abc093ea533aeb1
Karl Anderson <kra@monkey.org>
2021-08-16 10:53:28 -0700

    move menus to lua

--- asteriskserver/src/etc/asterisk/extensions.lua
+++ asteriskserver/src/etc/asterisk/extensions.lua
@@ -3,6 +3,7 @@ package.path = package.path .. ';/opt/asterisk/etc/asterisk/?.lua'
 extensions_admin = require("extensions_admin")
 extensions_challenge = require("extensions_challenge")
 extensions_holdthephone = require("extensions_holdthephone")
+extensions_misc = require("extensions_misc")
 extensions_outgoing = require("extensions_outgoing")
 extensions_wildcard_line = require("extensions_wildcard_line")
 
@@ -17,5 +18,6 @@ extensions = {
 for k,v in pairs(extensions_admin) do extensions[k] = v end
 for k,v in pairs(extensions_challenge) do extensions[k] = v end
 for k,v in pairs(extensions_holdthephone) do extensions[k] = v end
+for k,v in pairs(extensions_misc) do extensions[k] = v end
 for k,v in pairs(extensions_outgoing) do extensions[k] = v end
 for k,v in pairs(extensions_wildcard_line) do extensions[k] = v end
