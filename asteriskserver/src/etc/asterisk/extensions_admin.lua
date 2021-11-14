a0475db20066eb9b616e5570dd4e035162d1d680
Karl Anderson <kra@monkey.org>
2021-11-14 13:10:26 -0800

    move conf to lua

--- asteriskserver/src/etc/asterisk/extensions_admin.lua
+++ asteriskserver/src/etc/asterisk/extensions_admin.lua
@@ -5,8 +5,10 @@ local extensions = {
         {intro_statements={"fewtel"},
          menu_entries={
              [1]={"for-the-fewtel-voice-conference", "futel-conf"},
-             [2]={{"for", "the", "outgoing", "menus"}, "outgoing-chooser"},
-             [3]={"for-an-internal-dialtone", "internal-dialtone-wrapper"},
+             [2]={{"for", "the", "outgoing", "menus"},
+                 "outgoing_chooser"},
+             [3]={"for-an-internal-dialtone",
+                  "internal-dialtone-wrapper"},
              [4]={"to-record-a-menu", "record"},
              [0]={"for-the-operator", "operator"}},
          statement_dir=""}),
@@ -18,6 +20,15 @@ local extensions = {
              [3]={"to-record-a-menu", "record"},
              [4]={"for-the-wildcard-line", "wildcard_line_outgoing"},
              [0]={"for-the-operator", "operator"}},
+         statement_dir=""}),
+    outgoing_chooser = util.context(
+        {menu_entries={
+             [1]={
+                 {"for", "the", "portland", "menu"}, "outgoing_portland"},
+             [2]={{
+                     "for", "the", "ipsilanty", "menu"}, "outgoing_ypsi"},
+             [3]={{"for", "the", "souwester", "menu"},
+                 "outgoing_souwester"}},
          statement_dir=""})}
 
 return extensions
