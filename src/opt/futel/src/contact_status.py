fae501e492baec9db405679d56eb2dc2fa96327e
Karl Anderson <kra@monkey.org>
2022-10-04 17:17:59 -0700

    status publisher

--- src/opt/futel/src/contact_status.py
+++ src/opt/futel/src/contact_status.py
@@ -8,10 +8,8 @@ import re
 import subprocess
 
 # match a sip extension
-# "  Contact:  630/sip:630@192.168.56.1;transport=udp         9aa59cc6c8 Avail        11.957"
+# Contact:  630/sip:630@192.168.56.1;transport=udp         9aa59cc6c8 Avail        11.957
 extension_re = re.compile("([0-9][0-9][0-9])/sip:[0-9][0-9][0-9]")
-
-# lines = """
 
 #   Contact:  <Aor/ContactUri..............................> <Hash....> <Status> <RTT(ms)..>
 # ==========================================================================================
@@ -22,28 +20,43 @@ extension_re = re.compile("([0-9][0-9][0-9])/sip:[0-9][0-9][0-9]")
 
 # Objects found: 3
 
-# """
-
-# collect contact lines
-lines = subprocess.run(
-    ["/sbin/asterisk", "-cx", "pjsip show contacts"],
-    stdout=subprocess.PIPE)
-lines = [line.decode('ascii') for line in lines.stdout.splitlines()]
-lines = [line for line in lines
-         if line.strip().startswith("Contact")]
-
 Line = collections.namedtuple('Line', ['line', 'extension', 'status'])
 
-lines = [Line(line=line, extension=None, status=None) for line in lines]
-# filter for lines with extension
-lines = [Line(line=line.line, extension=extension_re.search(line.line), status=None)
-         for line in lines]
-lines = [line for line in lines if line.extension]
-# extract extension
-lines = [Line(line=line.line, extension=line.extension.group(1), status=None) for line in lines]
-# extract status
-lines = [Line(line=line.line, extension=line.extension, status=line.line.split()) for line in lines]
-lines = [Line(line=line.line, extension=line.extension, status=line.status[3]) for line in lines]
+def get_contact_lines():
+    """Return Lines from contact lines from asterisk."""
+    lines = subprocess.run(
+        ["/sbin/asterisk", "-cx", "pjsip show contacts"],
+        stdout=subprocess.PIPE)
+    lines = [line.decode('ascii') for line in lines.stdout.splitlines()]
+    lines = [line for line in lines
+             if line.strip().startswith("Contact")]
+    return [
+        Line(line=line, extension=None, status=None) for line in lines]
 
-for (line) in lines:
-    print(line.extension, line.status)
+def get_extension_lines(lines):
+    """Return filtered and populated Lines from lines."""
+    # filter for lines with extension
+    lines = [
+        Line(line=line.line,
+             extension=extension_re.search(line.line), status=None)
+        for line in lines]
+    lines = [line for line in lines if line.extension]
+    # extract extension
+    lines = [
+        Line(line=line.line,
+             extension=line.extension.group(1),
+             status=None) for line in lines]
+    # extract status
+    lines = [
+        Line(line=line.line,
+             extension=line.extension,
+             status=line.line.split()) for line in lines]
+    lines = [
+        Line(line=line.line,
+             extension=line.extension,
+             status=line.status[3]) for line in lines]
+    # we are done
+    return lines
+
+# for (line) in lines:
+#     print(line.extension, line.status)
