b9a5fcf41ac13b7cc94de1c2a6ac66fc80a6a07f
Karl Anderson <kra@monkey.org>
2022-10-09 11:45:35 -0700

    remove recording utility

--- src/etc/asterisk/extensions_admin.lua
+++ src/etc/asterisk/extensions_admin.lua
@@ -9,7 +9,6 @@ local extensions = {
                  "outgoing_chooser"},
              [3]={"for-an-internal-dialtone",
                   "internal-dialtone-wrapper"},
-             [4]={"to-record-a-menu", "record"},
              [0]={"for-the-operator", "operator"}},
          statement_dir=""}),
     member_main = util.context(
@@ -17,8 +16,7 @@ local extensions = {
          menu_entries={
              [1]={"for-the-fewtel-voice-conference", "futel-conf"},
              [2]={"for-an-internal-dialtone", "internal-dialtone-wrapper"},
-             [3]={"to-record-a-menu", "record"},
-             [4]={"for-the-wildcard-line", "wildcard_line_outgoing"},
+             [3]={"for-the-wildcard-line", "wildcard_line_outgoing"},
              [0]={"for-the-operator", "operator"}},
          statement_dir=""}),
     outgoing_chooser = util.context(
