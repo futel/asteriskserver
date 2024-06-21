8e09018f69111983621a0468ca7a43cc6be103bf
Karl Anderson <kra@monkey.org>
2024-06-20 20:24:12 -0700

    record calls

--- src/etc/asterisk/extensions_toorcamp.lua
+++ src/etc/asterisk/extensions_toorcamp.lua
@@ -17,9 +17,14 @@ function toorcamp_dial_context(destination_function)
     return context_array
 end
 
+-- call extension at shadytel endpoint, then metric with identifier
 function toorcamp_dial_outgoing(_context, exten)
     dialstring = util.get_dialstring(exten, 'shadytel-tcp')
     timeoutstring = util.get_timeoutstring(30) -- minutes
+    -- Begin monitoring call.
+    app.AGI("get_filename.agi")
+    filename = "outgoing-" .. channel.agi_out:get() .. ".wav"
+    app.MixMonitor(filename, "b")
     app.Dial(dialstring, nil, timeoutstring)
     dialstatus = channel.DIALSTATUS:get()
     -- we have completed the call, metric and react to the outcome    
@@ -35,11 +40,19 @@ end
 function toorcamp_dial_incoming(context, extension)
     util.metric(context)
     dialstring = util.get_dialstring(extension, nil)
-    -- XXX No timeout, will this DOS us or our upstream? But we want people
+    -- Begin monitoring call.
+    app.AGI("get_filename.agi")
+    filename = "incoming-" .. channel.agi_out:get() .. ".wav"
+    app.MixMonitor(filename, "b")
+    -- No timeout, will this DOS us or our upstream? But we want people
     -- to be able to leave an extension on speaker.
     app.Dial(dialstring)
     dialstatus = channel.DIALSTATUS:get()
     util.metric("incoming-dialstatus-" .. dialstatus .. "-" .. extension)
+    if (dialstatus == "CHANUNAVAIL") or (dialstatus == "CONGESTION") then
+        app.Playtones("congestion")
+        app.Congestion()
+    end
 end
 
 local extensions = {
