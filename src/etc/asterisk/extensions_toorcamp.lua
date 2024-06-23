e43bb3d1321e9e48e47573fcad0c2f847f075240
Karl Anderson <kra@monkey.org>
2024-06-23 14:10:05 -0700

    stub for incoming routing

--- src/etc/asterisk/extensions_toorcamp.lua
+++ src/etc/asterisk/extensions_toorcamp.lua
@@ -1,10 +1,14 @@
 util = require("util")
+math = require("math")
+math.randomseed(os.clock()*100000000000)
 
 function toorcamp_dialtone_outgoing(context, exten)
+    util.metric(context)
     app.DISA("no-password", "toorcamp_dial_outgoing")
 end
 
 function toorcamp_dialtone_incoming(context, exten)
+    util.metric(context)    
     app.DISA("no-password", "toorcamp_dial_incoming")
 end
 
@@ -17,6 +21,7 @@ function toorcamp_dial_context(destination_function)
     return context_array
 end
 
+-- Dial exten on outgoing_channel, metric.
 function toorcamp_dial(context, exten, outgoing_channel)
     util.metric(context)    
     dialstring = util.get_dialstring(exten, outgoing_channel)
@@ -36,21 +41,49 @@ function toorcamp_dial(context, exten, outgoing_channel)
     end
 end    
 
--- Dial a random extension on the outgoing channel. Intended to be used
--- by a call file.
+-- Continue with a random destination. Intended to be used by a call file.
 -- XXX Coming from a call file breaks the endpoint field of the metrics,
 --     util.py probably needs to be able to handle that.
-function toorcamp_dial_random_outgoing(context, exten)
-    ext = "6000"                -- XXX testing
-    return toorcamp_dial(context, ext, nil) -- XXX "shadytel-tcp"
+-- XXX Coming from a call file breaks callerid.
+-- XXX Can we wait for bridge to start? Will need that for conf?
+-- XXX Dial timeout?
+function toorcamp_dial_random(context, exten)
+    util.metric(context)    
+    parties = channel.CONFBRIDGE_INFO("parties", "666"):get()
+    if parties ~= "0" then
+        -- Conf is populated, send caller there.
+        return app.ConfBridge(
+            666, "futel_conf", "futel_conf_user", "futel_conf_menu")
+    else
+        -- Send caller to a random destination.
+        -- XXX The extension space is probably sparse.
+        --     We want to have a list of interesting numbers to prioritize:
+        --     previous incoming callers, previous recipients, just interesting.
+        rext = tostring(math.random(1000, 9999))
+        return toorcamp_dial_outgoing(context, rext)
+    end
 end
 
 function toorcamp_dial_outgoing(context, exten)
+    util.metric(context)
     return toorcamp_dial(context, exten, "shadytel-tcp")
 end
 
-function toorcamp_dial_incoming(context, extension)
-    return toorcamp_dial(context, exten, nil)
+function toorcamp_dial_incoming(context, exten)
+    util.metric(context)
+    util.metric("incoming_extension_" .. exten)
+    if exten == "3885" then     -- FUTL
+        fn = challenge_toorcamp_incoming
+    elseif exten == "2084" then
+        fn = toorcamp_dial_random
+    elseif exten == "4639" then -- HNDY
+        fn = toorcamp_dial_random
+    -- elseif exten == "5225" then -- JACK
+    --    fn = toorcamp_dial        
+    else    
+        fn = toorcamp_dial
+    end
+    return fn(context, exten, nil)
 end
 
 local extensions = {
@@ -61,11 +94,10 @@ local extensions = {
         toorcamp_dialtone_incoming),
     toorcamp_dial_incoming = toorcamp_dial_context(toorcamp_dial_incoming),
     toorcamp_dial_outgoing = toorcamp_dial_context(toorcamp_dial_outgoing),
-    toorcamp_dial_random_outgoing = util.destination_context(
-        toorcamp_dial_random_outgoing),
+    toorcamp_dial_random = util.destination_context(
+        toorcamp_dial_random),
     challenge_toorcamp_outgoing = util.context(
-        {intro_statements={},
-         menu_entries={
+        {menu_entries={
              {"to-make-a-call", "toorcamp_dialtone_outgoing"},             
              {"to-perform-the-challenges", "challenge_authenticate"},
              {"for-voicemail", "voicemail_outgoing"},
@@ -81,8 +113,7 @@ local extensions = {
          },
          statement_dir="challenge"}),
     challenge_toorcamp_incoming = util.context(
-        {intro_statements={},
-         menu_entries={
+        {menu_entries={
              {"to-perform-the-challenges", "challenge_authenticate"},
              {"for-voicemail", "voicemail_outgoing"},
              {"for-the-fewtel-voice-conference", "futel-conf"},
