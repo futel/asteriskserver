748f68fac6a93ed9cb51e5d35b8c07e59464652b
Karl Anderson <kra@monkey.org>
2024-07-04 23:54:41 -0700

    remove cruft

--- src/var/lib/asterisk/agi-bin/toorcamp_random_destination.py
+++ src/var/lib/asterisk/agi-bin/toorcamp_random_destination.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python3
-
-from asterisk import *
-import util
-
-import toorcamp_random_destination
-
-agi = agi.AGI()
-
-def main(agi_o, sequences, prefix_len):
+def main(agi_o):
     agi_o.goto_on_exit(
-        context='toorcamp_random_destination', extension='s')
-    #, priority='1')
+        context='toorcamp_random_destination',
+        extension='s')
